cfd0ceb0ac64ac113db291e17bf555827906d6d5
Journii-Hilliard <jhilliard@claflin.edu>
2025-10-21 16:13:38 -0400

    Add files via upload

---
+++ t-test.R
@@ -0,0 +1,21 @@
+#Author:Journii Hillard Date: 10/21/2025 Purpose:Calculate T-test
+
+#Create some dummy dataset for 2 variables
+
+x = rnorm(10)
+y = rnorm(10)
+
+#Create density plot
+ 
+pts = seq(-4.5,4.5,length=100)
+plot(pts,dt(pts,df=9),col='red',type='l')
+lines(density(x), col='green')
+lines(density(y), col='blue')
+
+#Calculate T-test
+
+ttest = t.test(x,y)
+
+#print value
+
+ttest
